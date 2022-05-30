198b10b0743fc9a7e2980c4a1850133411fd0fce
alphak3y <alphamik80@gmail.com>
2022-05-30 10:54:40 +0100

    update token prices using weighted average

--- src/mappings/pair.ts
+++ src/mappings/pair.ts
@@ -35,6 +35,7 @@ export function handleSync(event: Sync): void {
   //TODO: somehow get all the pairs that have been updated in the last x days
   //      and watch these contracts in the subgraph.yaml
   //NOTE: scraping through all the blocks to get every pair created is a bad idea
+  
   let loadExchangePair = safeLoadExchangePair(event.address.toHex())
   let loadPair = safeLoadPair(loadExchangePair.entity.token0, loadExchangePair.entity.token1)
   let loadExchange = safeLoadExchange(loadExchangePair.entity.exchange)
@@ -56,20 +57,32 @@ export function handleSync(event: Sync): void {
   let exPair = loadExchangePair.entity
   let exToken0 = loadExchangeToken0.entity
   let exToken1 = loadExchangeToken1.entity
+
+  // Bundle update
+  let ethUsdPrice = updateEthUsdPrice(exchange.id)
   
   // reset token total liquidity amounts
+  // Exchange
   exchange.totalLiquidityETH = exchange.totalLiquidityETH.minus(exPair.trackedReserveETH as BigDecimal)
+  exchange.totalLiquidityUSD = exchange.totalLiquidityUSD.minus(exPair.trackedReserveETH.times(ethUsdPrice))
+  // Token
   token0.totalLiquidity = token0.totalLiquidity.minus(exPair.reserve0)
   token1.totalLiquidity = token1.totalLiquidity.minus(exPair.reserve1)
+  // ExchangeToken
   exToken0.totalLiquidity = exToken0.totalLiquidity.minus(exPair.reserve0)
   exToken1.totalLiquidity = exToken1.totalLiquidity.minus(exPair.reserve1)
-
+  // Pair
   pair.reserve0 = pair.reserve0.minus(exPair.reserve0)
   pair.reserve1 = pair.reserve1.minus(exPair.reserve1)
   pair.reserveETH = pair.reserveETH.minus(exPair.reserveETH)
   pair.reserveUSD = pair.reserveUSD.minus(exPair.reserveUSD)
-  exPair.updatedAtTimestamp = event.block.timestamp
-  exPair.updatedAtBlockNumber = event.block.number
+  // TODO: calculate without this liquidity and add it back in
+  // Token
+  // - ethPrice
+  // - usdPrice
+  // Pair
+  // - token0Price
+  // - token1Price
 
   exPair.reserve0 = convertTokenToDecimal(event.params.reserve0, exToken0.decimals)
   exPair.reserve1 = convertTokenToDecimal(event.params.reserve1, exToken1.decimals)
@@ -82,13 +95,50 @@ export function handleSync(event: Sync): void {
   else exPair.token1Price = BIGDECIMAL_ZERO
 
   //update weighted average price for cross-exchange token entity
-
   exPair.save()
 
-  let ethUsdPrice = updateEthUsdPrice(exchange.id)
+  exToken0 = updateExchangeTokenPrices(exToken0, ethUsdPrice)
+  exToken1 = updateExchangeTokenPrices(exToken1, ethUsdPrice)
 
-  updateEthBasedPrices(exToken0, exToken1, token0, token1, exPair, exchange, ethUsdPrice)
+  exToken0.save()
+  exToken1.save()
 
+  token0.ethPrice = findEthPerToken(token0)
+  token1.ethPrice = findEthPerToken(token1)
+
+  exPair = updateTwoPercentMarketDepth(exPair, exToken1, ethUsdPrice)
+
+  let exchangePairTrackedLiquidityETH = getExchangePairTrackedLiquidityETH(ethUsdPrice, exPair, exToken0, exToken1)
+
+  // add new liquidity values
+  
+  // Exchange
+  exchange.totalLiquidityETH = exchange.totalLiquidityETH.plus(exchangePairTrackedLiquidityETH)
+  exchange.totalLiquidityUSD = exchange.totalLiquidityETH.times(ethUsdPrice)
+  // ExchangeToken
+  exToken0.totalLiquidity = exToken0.totalLiquidity.plus(exPair.reserve0)
+  exToken1.totalLiquidity = exToken1.totalLiquidity.plus(exPair.reserve1)
+  // Token using exPair.reserve0/1
+  // - ethPrice
+  // - usdPrice
+  // ExchangePair
+  exPair.trackedReserveETH = exchangePairTrackedLiquidityETH
+  exPair.reserveETH = exPair.reserve0
+    .times(exToken0.ethPrice as BigDecimal)
+    .plus(exPair.reserve1.times(exToken1.ethPrice as BigDecimal))
+  exPair.reserveUSD = exPair.reserveETH.times(ethUsdPrice)
+  // Pair using exPair.reserveETH/USD
+  // - token0Price
+  // - token1Price
+
+  // save entities
+  exchange.save()
+  exPair.save()
+  pair.save()
+  exToken0.save()
+  exToken1.save()
+  token0.save()
+  token1.save()
 }
 
 export function updateEthUsdPrice(factoryAddress: string): BigDecimal {
@@ -104,48 +154,44 @@ export function updateEthUsdPrice(factoryAddress: string): BigDecimal {
   return ethUsdPrice.value
 }
 
-export function updateEthBasedPrices(exToken0: ExchangeToken, exToken1: ExchangeToken, token0: Token, token1: Token, exPair: ExchangePair, exchange: Exchange, ethUsdPrice: BigDecimal): void {
-  exToken0.ethPrice = findEthPerExchangeToken(exToken0)
-  exToken1.ethPrice = findEthPerExchangeToken(exToken1)
+export function updateExchangeTokenPrices(exToken: ExchangeToken, ethUsdPrice: BigDecimal): ExchangeToken {
+  exToken.ethPrice = findEthPerExchangeToken(exToken)
   ///get exchange tokens and update prices
-  exToken0.usdPrice = findUsdPerExchangeToken(exToken0, ethUsdPrice)
-  exToken1.usdPrice = findUsdPerExchangeToken(exToken1, ethUsdPrice)
-  //also update weighted average prices
-  exToken0.save()
-  exToken1.save()
+  exToken.usdPrice = findUsdPerExchangeToken(exToken, ethUsdPrice)
 
-  token0.ethPrice = findEthPerToken(token0)
+  return exToken
+}
 
+export function updateTwoPercentMarketDepth(exPair: ExchangePair, exToken1: ExchangeToken, ethUsdPrice: BigDecimal): ExchangePair {
   exPair.twoPercentMarketDepthETH = exPair.reserve1.times(BIGDECIMAL_ONE_PERCENT).times(exToken1.ethPrice)
   exPair.twoPercentMarketDepthUSD = exPair.twoPercentMarketDepthETH.times(ethUsdPrice)
+  return exPair
+}
 
-  // get tracked liquidity - will be 0 if neither is in whitelist
-  let trackedLiquidityETH: BigDecimal
-  if (ethUsdPrice.notEqual(BIGDECIMAL_ZERO)) {
-    trackedLiquidityETH = getExchangeTrackedLiquidityUSD(exPair.reserve0, exToken0, exPair.reserve1, exToken1).div(
-      ethUsdPrice
-    )
-  } else {
-    trackedLiquidityETH = BIGDECIMAL_ZERO
-  }
+export function getExchangePairTrackedLiquidityETH(ethUsdPrice: BigDecimal, exPair: ExchangePair, exToken0: ExchangeToken, exToken1: ExchangeToken): BigDecimal {
+    // get tracked liquidity - will be 0 if neither is in whitelist
+    let trackedLiquidityETH: BigDecimal
+    if (ethUsdPrice.notEqual(BIGDECIMAL_ZERO)) {
+      trackedLiquidityETH = getExchangeTrackedLiquidityUSD(exPair.reserve0, exToken0, exPair.reserve1, exToken1).div(
+        ethUsdPrice
+      )
+    } else {
+      trackedLiquidityETH = BIGDECIMAL_ZERO
+    }
+    return trackedLiquidityETH
+}
 
-  // add new liquidity values
-  exchange.totalLiquidityETH = exchange.totalLiquidityETH.plus(trackedLiquidityETH)
-  exchange.totalLiquidityUSD = exchange.totalLiquidityETH.times(ethUsdPrice)
 
-  exPair.trackedReserveETH = trackedLiquidityETH
-  exPair.reserveETH = exPair.reserve0
-    .times(exToken0.ethPrice as BigDecimal)
-    .plus(exPair.reserve1.times(exToken1.ethPrice as BigDecimal))
-  exPair.reserveUSD = exPair.reserveETH.times(ethUsdPrice)
+export function updateEthBasedPrices(exToken0: ExchangeToken, exToken1: ExchangeToken, token0: Token, token1: Token, exPair: ExchangePair, exchange: Exchange, ethUsdPrice: BigDecimal): void {
 
-  // now correctly set liquidity amounts for each token
-  exToken0.totalLiquidity = exToken0.totalLiquidity.plus(exPair.reserve0)
-  exToken1.totalLiquidity = exToken1.totalLiquidity.plus(exPair.reserve1)
+  //also update weighted average prices
 
-  // save entities
-  exPair.save()
-  exchange.save()
-  exToken0.save()
-  exToken1.save()
+
+
+  
+
+
+
+
+
 }
